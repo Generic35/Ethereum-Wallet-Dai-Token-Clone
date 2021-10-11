7c2d83e7c173231fcce3caabf579ef9975572479
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-11 18:42:17 -0400

    feat: Add the mintable token (the mock Dai token

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+	const tokenMock = await DaiTokenMock.deployed();
+
+	await tokenMock.mint('0x0995aA3856C52Fb841be1104642D3Fe116d79051', '1000000000000000000000')
 };
